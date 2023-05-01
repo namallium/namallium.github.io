e5a843a287ec2208e8e07a370e740b1e7ad570ab
Namrata <namratajkulkarni@gmail.com>
2023-05-01 18:59:01 -0400

    updated data

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -4,48 +4,42 @@ const positions = [
     position: 'Web Developer',
     link: '',
     daterange: 'April 2023 - Present',
-    points: [
-    ],
+    points: [],
   },
   {
     company: 'Contract',
     position: 'UI/UX Designer',
     link: '',
     daterange: 'Oct 2022 - April 2023',
-    points: [
-    ],
+    points: [],
   },
   {
     company: 'Pennsylvania State University',
     position: 'Teaching Assistant',
     link: '',
     daterange: 'Jan 2022 - May 2022',
-    points: [
-    ],
+    points: [],
   },
   {
     company: 'Pennsylvania State University',
     position: 'Research Assistant',
     link: '',
     daterange: 'Mar 2021 - May 2022',
-    points: [
-    ],
+    points: [],
   },
   {
     company: 'Freelancing',
     position: 'Web Developer, Designer',
     link: '',
     daterange: 'June 2019 - Dec 2020',
-    points: [
-    ],
+    points: [],
   },
   {
     company: 'Bizsense Solutions Pvt. Ltd.',
     position: 'Web Developer',
     link: '',
     daterange: 'March 2017 - May 2019',
-    points: [
-    ],
+    points: [],
   },
 ];
 
